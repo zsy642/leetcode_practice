c9cc396097838172db4a3d46a4868088f9bb7682
zsy642 <13846053645@163.com>
2026-03-19 21:36:37 +0800

    27我的的答案版本(快慢指针法,优雅)

--- answer_27.py
+++ answer_27.py
@@ -1,19 +1,12 @@
 from typing import List
 def removeElement(nums, val):
-    left = 0
-    right = len(nums) - 1
-
-    while left <= right:
-        if nums[left] == val:
-            # 如果左边是我们要删的，就把最右边的元素挪过来顶包
-            nums[left] = nums[right]
-            # 右边那个位置已经处理过了（挪走了），所以右边界向内缩
-            right -= 1
-        else:
-            # 如果左边是正常的，就放心跳过，看下一个
-            left += 1
-
-    return left
+    slow=0
+    fast=0
+    for num in nums:
+        if num!=val:
+            nums[slow]=num
+            slow+=1
+    return slow
 
 def main():
     return 0
