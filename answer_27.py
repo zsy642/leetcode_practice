7900ae9e7afb746c7b885cca1b06b3a31516fce0
zsy642 <13846053645@163.com>
2026-03-19 20:48:40 +0800

    27自己的答案版本(自己想的对撞指针法,另外补充了一个极端案例)

--- answer_27.py
+++ answer_27.py
@@ -1,10 +1,16 @@
 from typing import List
 def removeElement( nums: List[int], val: int) -> int:
-    try:
-        while True:
-            nums.remove(val)
-    except ValueError:
-        return len(nums)
+    if nums==[]:
+        return 0
+    i=0
+    j=len(nums)-1
+    while i!=j:
+        if nums[i]==val:
+            nums[i],nums[j]=nums[j],nums[i]
+            j-=1
+        else:
+            i+=1
+    return i+1 if nums[i]!=val else i
 def main():
     return 0
     
--- test_my.py
+++ test_my.py
@@ -2,14 +2,16 @@ import pytest
 from answer_27 import removeElement
 @pytest.mark.parametrize('nums,val,new_nums,lenght',[
     ([1,1,1,1,1,11,1,1,1,1,11,1,1,1,11,1,2,1],1,[11,11,11,2],4),
-    ([1,2,3,3,4,5,6],3,[1,2,4,5,6],5)
+    ([1,2,3,3,4,5,6],3,[1,2,4,5,6],5),
+    ([],2,[],0)
 ])
 
 def test_removeElement(nums,val,new_nums,lenght):
     assert removeElement(nums,val)==lenght
-    nums.sort
-    new_nums.sort
-    for i in range (len(nums)):
+    nums=nums[:lenght]
+    nums.sort()
+    new_nums.sort()
+    for i in range (lenght-1):
         assert nums[i]==new_nums[i]
 
 if __name__ == '__main__':
