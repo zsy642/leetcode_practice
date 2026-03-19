08a623fa5bfcb1f6ec9868e0cf72bb406c00b7b2
zsy642 <13846053645@163.com>
2026-03-20 00:54:51 +0800

    977自己的答案(虽然自己写的但是和代码随想录一模一样)

--- answer_977.py
+++ answer_977.py
@@ -1,30 +1,18 @@
 from typing import List
 def sortedSquares(nums: List[int]) -> List[int]:
-    nums1=[]
-    nums2=[]
     head=0
     tail=len(nums)-1
+    new_nums=[None]*len(nums)
+    new_tail=len(nums)-1
     while head<=tail:
-        if nums[head]<=0:
-            nums1.append(nums[head]**2)
+        if nums[head]**2>=nums[tail]**2:
+            new_nums[new_tail]=nums[head]**2
             head+=1
-        elif nums[tail]>0:
-            nums2.append(nums[tail]**2)
+        else :
+            new_nums[new_tail]=nums[tail]**2
             tail-=1
-    i=0
-    while nums1!=[] and nums2!=[]:
-        if nums1[-1]>= nums2[-1]:
-            nums[i]=nums2[-1]
-            nums2.pop()
-        else:
-           nums[i]=nums1[-1]
-           nums1.pop()
-        i+=1
-    if nums1==[]:
-        nums[i:]=nums2[::-1]
-    else:
-        nums[i:]=nums1[::-1]
-    return nums
+        new_tail-=1
+    return new_nums
 
 def main():
     return 0
