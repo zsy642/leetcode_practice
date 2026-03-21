3a21ad9ae2cbd1c64be140b5c5ae4e8767bc65c7
zsy642 <13846053645@163.com>
2026-03-21 15:22:38 +0800

    59代码随想录的答案版本(循环,而且非常优雅)

--- answer_59.py
+++ answer_59.py
@@ -1,27 +1,34 @@
 from typing import List
-def generateMatrix( n: int) -> List[List[int]]:
-    res=[[None]*n for _ in range (n)]
-    def real_gene(res,n,i=0,j=0,num=1):
-        head=i
-        tail=j
-        if n<1:
-            return
-        for j in range (tail,n):
-            res[i][j]=num
-            num+=1
-        for i in range (head+1,n):
-            res[i][j]=num
-            num+=1
-        for j in range (n-2,tail-1,-1):
-            res[i][j]=num
-            num+=1
-        for i in range (n-2,head,-1):
-            res[i][j]=num
-            num+=1
-        real_gene(res,n-1,i,j+1,num)
-    real_gene(res,n)
-    return res
-
+def generateMatrix(n):
+    if n <= 0:
+        return []
+    # 初始化 n x n 矩阵
+    matrix = [[0]*n for _ in range(n)]
+    # 初始化边界和起始值
+    top, bottom, left, right = 0, n-1, 0, n-1
+    num = 1
+    while top <= bottom and left <= right:
+        # 从左到右填充上边界
+        for i in range(left, right + 1):
+            matrix[top][i] = num
+            num += 1
+        top += 1
+        # 从上到下填充右边界
+        for i in range(top, bottom + 1):
+            matrix[i][right] = num
+            num += 1
+        right -= 1
+        # 从右到左填充下边界
+        for i in range(right, left - 1, -1):
+            matrix[bottom][i] = num
+            num += 1
+        bottom -= 1
+        # 从下到上填充左边界
+        for i in range(bottom, top - 1, -1):
+            matrix[i][left] = num
+            num += 1
+        left += 1
+    return matrix
 
 
 
