9b4aed8cbebb026a57ca7cbd3d7e362e20b19b00
zsy642 <13846053645@163.com>
2026-04-03 06:16:09 +0800

    answer_242代码随想录的答案版本 Python 中的 Counter 是 collections 模块中的一个子类，专门用于统计可迭代对象中各元素出现的次数。 它的本质是一个字典 (dict)，其中元素作为“键 (key)”，出现次数作为“值 (value)”。

--- answer_242.py
+++ answer_242.py
@@ -1,12 +1,6 @@
-class Solution:
+class Solution(object):
     def isAnagram(self, s: str, t: str) -> bool:
-        from collections import defaultdict
-
-        s_dict = defaultdict(int)
-        t_dict = defaultdict(int)
-        for x in s:
-            s_dict[x] += 1
-
-        for x in t:
-            t_dict[x] += 1
-        return s_dict == t_dict
+        from collections import Counter
+        a_count = Counter(s)
+        b_count = Counter(t)
+        return a_count == b_count
