c3af3d967ef980d185a0f38f262c801c2f19b53e
zsy642 <13846053645@163.com>
2026-04-03 23:52:14 +0800

    answer_349我的答案版本

---
+++ answer_349.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def intersection(nums1: List[int], nums2: List[int]) -> List[int]:
+    res=[]
+    nums2=set(nums2)
+    for x in nums2:
+        if x in nums1:
+            res.append(x)
+    return res
+def main():
+    nums1=[2,2]
+    nums2=[2,2]
+    res=intersection(nums1,nums2)
+    return 0
+    
+if __name__ == '__main__':
+    main()
