2b2e1ceb9efd7b9262ffae900025c731994b8633
zsy642 <13846053645@163.com>
2026-04-04 00:42:07 +0800

    answer_349gemini的优化的答案版本

--- answer_349.py
+++ answer_349.py
@@ -4,6 +4,7 @@ from typing import List
 def intersection(nums1: List[int], nums2: List[int]) -> List[int]:
     res=[]
     nums2=set(nums2)
+    nums1=set(nums1)
     for x in nums2:
         if x in nums1:
             res.append(x)
