8328a8b5c9eb7ab6c12100ad7e2ca33cbc647f6e
zsy642 <13846053645@163.com>
2026-04-18 22:21:27 +0800

    ka_answer_54 我的答案版本(双指针底层版本)

--- ka_answer_54.py
+++ ka_answer_54.py
@@ -1,9 +1,22 @@
 import sys
-
-import re
-
 s=sys.stdin. read().split()[0]
-
-s=re.sub(r'\d','number',s)
-
+s=list(s)
+l=len(s)
+ex=0
+for i in s:
+    if i>='0' and i<='9':
+        ex+=5
+for _ in range(ex):
+    s.append('a')
+tail=l+ex-1
+head=l-1
+while head<tail:
+    if '0' <= s[head] <= '9':
+        s[tail-5:tail+1]=['n','u','m','b','e','r']
+        tail-=6
+    else :
+        s[tail]=s[head]
+        tail-=1
+    head-=1
+s=''.join(s)
 print(s)
