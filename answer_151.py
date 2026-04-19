cf65a82d75c05598c8ba67227773adf299a3f08d
zsy642 <13846053645@163.com>
2026-04-20 00:56:14 +0800

    ka_answer_151 我的答案版本(正确) 第二种答案实现了原地修改但不够优雅

--- answer_151.py
+++ answer_151.py
@@ -2,15 +2,25 @@ def reverseWords(s: str) -> str:
     s=s.strip()
     s=list(s)
     tmp=''
-    new_s=[]
-    for i in range(len(s)-1,-1,-1):
-        if s[i]==' ':
-            if tmp:
-                new_s.append(tmp[::-1])
-            tmp=''
-        else:
-            tmp+=s[i]
-    new_s.append(tmp[::-1])
-    new_s=' '.join(new_s)
-    return new_s
+    head=0
+    tail=0
+    real=0
+    s=s[::-1]
+    while head<len(s):
+        tail+=1
+        if tail==len(s) or s[tail]==' ':
+            if head!=0:
+                s[real:real+tail-head]=s[tail-1:head-1:-1]
+            else:
+                s[real:real+tail-head]=s[tail-1::-1]
+            if tail<=len(s)-1:
+                s[real+tail-head]=' '
+            real+=(tail-head+1)
+            while tail<len(s)-1 and s[tail]==' ':
+                tail+=1
+            head=tail
+    s=s[0:real+tail-head-1]
+    s=''.join(s)
+    return s
+
 
