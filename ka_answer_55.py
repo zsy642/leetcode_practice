e87ccf00a2b4f0cdc22497685febb26519b4f58c
zsy642 <13846053645@163.com>
2026-04-24 21:45:42 +0800

    ka_answer_55 代码随想录的答案版本(语法极致优雅,但不满足O(1)空间复杂度) python多用切片,但切片会先开辟新空间装

--- ka_answer_55.py
+++ ka_answer_55.py
@@ -1,27 +1,10 @@
-import sys
+#获取输入的数字k和字符串
+k = int(input())
+s = input()
 
-
-def abbb(s,target):
-    #import sys
-    #date=sys.stdin.read().split()
-    #target=int(date[0])
-    s=list(s)
-    #s=list(date[1])
-    o=len(s)-1
-    h=o-target
-    i=0 #
-    while h>-1:
-        s[h],s[o]=s[o],s[h]
-        h-=1
-        o-=1
-        i+=1
-    h=i%target-1
-    while o>0 and (len(s)%target !=0):
-        s[h],s[o]=s[o],s[h]
-        h=abs((h-1)%(i%target))
-        o-=1
-    s=''.join(s)
-    return s
-
+#通过切片反转第一段和第二段字符串
+#注意：python中字符串是不可变的，所以也需要额外空间
+s = s[len(s)-k:] + s[:len(s)-k]
+print(s)
 if __name__ == '__main__':
     main()
