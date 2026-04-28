e0bb0fc85b530e43b5584a7e9380a47a85df32a8
zsy642 <13846053645@163.com>
2026-04-29 01:16:41 +0800

    answer_28 我的答案版本(kmp法)这部分看=gemini聊天记录学

--- answer_28.py
+++ answer_28.py
@@ -1,15 +1,32 @@
+# leetcode submit region begin(Prohibit modification and deletion)
+def one_nums(s):
+    l=len(s)
+    one_next=[0]*l
+    j=0
+    for i in range(1,len(s)):
+        if j>0 and s[i]!=s[j]:
+            j=one_next[j-1]
+        if s[i]==s[j]:
+            j+=1
+            one_next[i]=j
+    return one_next
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        one_next=one_nums(needle)
+        j=0
+        i=0
+        while i<len(haystack):
+            if haystack[i]==needle[j]:
+                j+=1
+                i+=1
+                if j==len(needle):
+                    return (i-len(needle))
+            else:
+                if j==0:
+                    i+=1
+                else:
+                    j=one_next[j-1]
+        return -1
 
-def strStr(haystack: str, needle: str) -> int:
-    s=needle[0]
-    index=-1
-    for i in range(len(haystack)):
-        if haystack[i]==s:
-            index=i
-            for j in needle:
-                if index>(len(haystack)-1) or j !=haystack[index]:
-                    index=-1
-                    break
-                index+=1
-            if index!=-1:
-                return i
+# leetcode submit region end(Prohibit modification and deletion)
 
