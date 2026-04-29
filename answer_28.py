8433af4017eaa9b9c7ab37c0cbddfb02816d4680
zsy642 <13846053645@163.com>
2026-04-29 23:54:24 +0800

    answer_28 我的答案优雅重构版本(kmp法)比原版优雅多了,重点看while和kmp的next数组和下面主循环的同构写法,知识点:三元控制语句,和enumerate(这个其实没有用但为了知识点写了)

--- answer_28.py
+++ answer_28.py
@@ -4,29 +4,23 @@ def one_nums(s):
     one_next=[0]*l
     j=0
     for i in range(1,len(s)):
-        if j>0 and s[i]!=s[j]:
+        while j>0 and s[i]!=s[j]:
             j=one_next[j-1]
         if s[i]==s[j]:
             j+=1
             one_next[i]=j
     return one_next
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        one_next=one_nums(needle)
-        j=0
-        i=0
-        while i<len(haystack):
-            if haystack[i]==needle[j]:
-                j+=1
-                i+=1
-                if j==len(needle):
-                    return (i-len(needle))
-            else:
-                if j==0:
-                    i+=1
-                else:
-                    j=one_next[j-1]
-        return -1
 
-# leetcode submit region end(Prohibit modification and deletion)
+def strStr( haystack: str, needle: str) -> int:
+    one_next=one_nums(needle)
+    j=0
+    for i ,v in enumerate(haystack):
+        while j>0 and haystack[i]!=needle[j]:
+            j=one_next[j-1]
+        if haystack[i]==needle[j]:
+            j+=1
+            if j==len(needle):
+                return (i-len(needle)+1)
 
+    return -1
+# letcode submit region end(Prohibit modification and deletion)
