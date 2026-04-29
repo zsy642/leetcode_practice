275d102b40a89c0d1c48cbc1969d9ae3cb2a4dee
zsy642 <13846053645@163.com>
2026-04-30 01:00:20 +0800

    answer_459 我的答案版本(数学证明+kmp的力量)

--- answer_459.py
+++ answer_459.py
@@ -1,2 +1,21 @@
+def one_nums(s):
+    l=len(s)
+    one_next=[0]*l
+    j=0
+    for i in range(1,len(s)):
+        while j>0 and s[i]!=s[j]:
+            j=one_next[j-1]
+        if s[i]==s[j]:
+            j+=1
+            one_next[i]=j
+    return one_next
+
 def repeatedSubstringPattern( s: str) -> bool:
-    return s in (s + s)[1:-1]
+    if len(s)==1:
+        return False
+    one_next=one_nums(s)
+    rel=len(s)-one_next[-1]
+    if one_next[-1]>=rel and one_next[-1]%rel==0:
+        return True
+    else:
+        return False
