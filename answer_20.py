2c2684294d0e0cbc859b2e9edb1ae7c0fa4637b8
zsy642 <13846053645@163.com>
2026-05-01 03:00:22 +0800

    answer_20 gemini改的我的答案版本 注意他把字典的妙用

--- answer_20.py
+++ answer_20.py
@@ -1,17 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack=[]
-        for i in s:
-            if i=='(':
-                stack.append(')')
-            elif i=='[':
-                stack.append(']')
-            elif i=='{':
-                stack.append('}')
+        # 用字典管理匹配规则，想加多少种括号加多少种
+        mapping = {'(': ')', '[': ']', '{': '}'}
+        stack = []
+
+        for char in s:
+            if char in mapping:
+                # 遇到左括号，把对应的右括号压栈
+                stack.append(mapping[char])
             else:
-                if not stack or i != stack[-1]:
+                # 遇到右括号，进行验证
+                if not stack or char != stack[-1]:
                     return False
                 stack.pop()
-        if stack:
-            return False
-        return True
+
+        return not stack  # 等同于 if stack: return False else: return True
