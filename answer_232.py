9388fec93a01ef8718afe29f68806357c544f725
zsy642 <13846053645@163.com>
2026-04-30 14:28:57 +0800

    answer_232 gemini的答案版本(时间复杂度O(1))

--- answer_232.py
+++ answer_232.py
@@ -1,5 +1,4 @@
 class MyQueue:
-
     def __init__(self):
         self.old=[]
         self.new=[]
@@ -7,23 +6,15 @@ class MyQueue:
         self.old.append(x)
 
     def pop(self) -> int:
-        self.new=self.old[::-1]
-        tmp=self.new.pop()
-        self.old=self.new[::-1]
-        self.new=[]
-        return tmp
+        if not self.new:
+            self.new=self.old[::-1]
+            self.old=[]
+        return self.new.pop()
 
     def peek(self) -> int:
-        self.new=self.old[::-1]
-        tmp=self.new[-1]
-        self.new=[]
-        return tmp
+        if not self.new:
+            self.new=self.old[::-1]
+            self.old=[]
+        return self.new[-1]
     def empty(self) -> bool:
-        return len(self.old)!=0
-
-x=MyQueue()
-x.push(2)
-x.push(3)
-x.pop()
-x.pop()
-print(x.peek())
+        return (len(self.old)+len(self.new))==0
