8b1f6c8b3e8b125c252d9505372adc9e69da950a
zsy642 <13846053645@163.com>
2026-05-03 19:53:49 +0800

    answer_239 gemini的答案版本(优雅) 只维护有可能的数据

--- answer_239.py
+++ answer_239.py
@@ -1,23 +1,23 @@
 from collections import deque
-def maxSlidingWindow(nums, k: int) :
-    queque = deque()
-    maybe = deque()
-    maybe_i = deque()
-    maybe.append(float('-inf'))
-    maybe_i.append(-3.14)
+
+
+def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+    # 只要这一个“砖头”就够了，存索引
+    dq = deque()
     res = []
-    for i, value in enumerate(nums):
-        if len(queque) <= k:
-            queque.append(value)
-            while maybe!=deque() and value >= maybe[-1]:
-                maybe.pop()
-                maybe_i.pop()
-            maybe.append(value)
-            maybe_i.append(i)
-            if maybe_i[0] < (i - k + 1):
-                maybe.popleft()
-                maybe_i.popleft()
-        if len(queque) >= k:
-            res.append(maybe[0])
-            queque.popleft()
+
+    for i, val in enumerate(nums):
+        # 1. 谁比我弱，谁就走（保持单调递减）
+        while dq and nums[dq[-1]] <= val:
+            dq.pop()
+        dq.append(i)
+
+        # 2. 谁太老了，谁就走（判断索引是否过期）
+        if dq[0] == i - k:
+            dq.popleft()
+
+        # 3. 窗口成型，记录老大
+        if i >= k - 1:
+            res.append(nums[dq[0]])
+
     return res
