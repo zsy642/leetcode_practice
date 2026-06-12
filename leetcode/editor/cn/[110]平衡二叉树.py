3be8f6859ad3d60cf3258da1d6b379aa13bd8a33
zsy642 <13846053645@163.com>
2026-06-12 23:00:05 +0800

    answer_220 我的答案版本,暴力解法(其实这是最优解),我去掉了乱七八糟的注释

--- leetcode/editor/cn/[110]平衡二叉树.py
+++ leetcode/editor/cn/[110]平衡二叉树.py
@@ -44,34 +44,22 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        #checkBalanced(root)-> int #返回值 返回一边的高度
-            #获取左右的高度
-            #如果绝对值>1直接返回999
-            #否则继续向上递归
-        #调用checkBalaned,如果小于
-        if not root:
-            return True
-            # finddepth形参节点,返回值深度(算自己也算叶子)和底层叶子数
 
-        def finddepth(root):
-            # 退出条件如果是叶子返回1如果是一个叶子返回2
-            if not root:
+        def finddepth(node):
+            if not node:
                 return 0
-            if not root.left and not root.right:
-                return 1
 
-            # 每次先左调用一次,判断没有叶子再进行右边调用,否则直接返回高度和叶子数
-            deepl = finddepth(root.left)
-            deepr = finddepth(root.right)
-            if  deepl==-1 or deepr==-1:
-                return -1
-            if abs(deepl-deepr)>1:
-                return -1
-            return max(deepl,deepr) + 1
+            deepl = finddepth(node.left)
+            if deepl == -1: return -1  # 左子树不平衡，立刻熔断
 
-        if (finddepth(root))==-1:
-            return False
-        else:
-            return True
+            deepr = finddepth(node.right)
+            if deepr == -1: return -1  # 右子树不平衡，立刻熔断
+
+            if abs(deepl - deepr) > 1:
+                return -1
+
+            return max(deepl, deepr) + 1
+
+        return finddepth(root) != -1
 
 # leetcode submit region end(Prohibit modification and deletion)
