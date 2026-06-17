4a063702b21eec6a3d131d03fb46a00f9252d8fc
zsy642 <13846053645@163.com>
2026-06-18 06:50:14 +0800

    answer_513 gemini的答案版本 还是dfs

--- leetcode/editor/cn/[513]找树左下角的值.py
+++ leetcode/editor/cn/[513]找树左下角的值.py
@@ -44,18 +44,24 @@
 #         self.right = right
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        res=(0,0)
-        def dfs(root,num=0):
-            nonlocal res
-            num+=1
-            if not root.left and not root.right :
-                if res[1]<num:
-                    res=(root.val,num)
-            if root.left:
-                dfs(root.left,num)
-            if root.right:
-                dfs(root.right,num)
+        # res 存 (节点值, 最大深度)
+        # 初始深度给 1，因为 root 必然存在（题目给出了不为空的限制）
+        res = (root.val, 1)
 
-        dfs(root, num=0)
+        def dfs(node, depth):
+            nonlocal res
+            if not node:
+                return
+
+            # 中：如果是叶子节点，且破了深度纪录，直接占坑
+            if not node.left and not node.right:
+                if depth > res[1]:
+                    res = (node.val, depth)
+
+            # 左、右：下钻时直接把 depth + 1 传下去，不改变当前层的 depth
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+
+        dfs(root, 1)
         return res[0]
 # leetcode submit region end(Prohibit modification and deletion)
