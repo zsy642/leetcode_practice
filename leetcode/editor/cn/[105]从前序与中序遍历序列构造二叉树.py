100b85e3906b2100424c127cf5a1f91c1a16c645
zsy642 <13846053645@163.com>
2026-07-01 10:17:27 +0800

    105,106 gemini的答案版本 清晰，优雅。 把你的 inorder.index 换成了字典，把你的数组切片换成了数字加减。优化了时间和空间复杂度。

--- leetcode/editor/cn/[105]从前序与中序遍历序列构造二叉树.py
+++ leetcode/editor/cn/[105]从前序与中序遍历序列构造二叉树.py
@@ -43,16 +43,37 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder:
-            return
-        root=TreeNode()
-        root.val=preorder[0]
-        myindex=inorder.index(root.val)
-        linorder=inorder[:myindex]
-        rinorder=inorder[myindex+1:]
-        lpreorder = preorder[1:myindex+1]
-        rpreorder = preorder[myindex+1:]
-        root.left=self.buildTree(lpreorder,linorder)
-        root.right=self.buildTree(rpreorder,rinorder)
-        return root
+        # 1. 用哈希表给中序遍历建一个“快速索引字典”，实现 O(1) 查找
+        inorder_map = {val: i for i, val in enumerate(inorder)}
+
+        # 2. 闭包函数，只传四个指针，死死咬住当前子树在原数组里的范围
+        def myBuildTree(pre_left, pre_right, in_left, in_right):
+            # 边界条件：指针错位了，说明没有节点了
+            if pre_left > pre_right:
+                return None
+
+            # 前序遍历的第一个元素是根节点
+            root_val = preorder[pre_left]
+            root = TreeNode(root_val)
+
+            # O(1) 查出根节点在中序遍历中的位置
+            in_root_index = inorder_map[root_val]
+
+            # 算出左子树的节点个数，这是原数组里的“黄金跨度”
+            left_subtree_size = in_root_index - in_left
+
+            # 递归分配任务，纯指针算术，零内存拷贝：
+            root.left = myBuildTree(
+                pre_left + 1, pre_left + left_subtree_size,
+                in_left, in_root_index - 1
+            )
+            root.right = myBuildTree(
+                pre_left + left_subtree_size + 1, pre_right,
+                in_root_index + 1, in_right
+            )
+
+            return root
+
+        n = len(preorder)
+        return myBuildTree(0, n - 1, 0, n - 1)
 # leetcode submit region end(Prohibit modification and deletion)
